615285bd1a837249e42d6e851b3c7bdaefe7b248
Icaro-Lima <icaro.lima@ccc.ufcg.edu.br>
2018-08-17 17:59:04 -0300

    fazendo integração com google maps

---
+++ Assets/Scripts/Camera/CameraAsBackground.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CameraAsBackground : MonoBehaviour {
+
+    private RawImage image;
+    private WebCamTexture cam;
+
+	// Use this for initialization
+	void Start () {
+        image = GetComponent<RawImage>();
+        cam = new WebCamTexture(Screen.width, Screen.height);
+        image.texture = cam;
+        cam.Play();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/Camera/GyroCamera.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GyroCamera : MonoBehaviour
+{
+    // Use this for initialization
+    void Start()
+    {
+        Input.compass.enabled = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0, Input.compass.magneticHeading, 0), Time.deltaTime * 2);
+    }
+}
